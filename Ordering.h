825eb7612da6f5546491214bc33b4bfba6804e7c
DobrianskyiVol <dobrianskyi.volodymyr@chnu.edu.ua>
2024-03-27 14:31:34 +0200

    Add fields user, order, status

--- Ordering.h
+++ Ordering.h
@@ -4,9 +4,16 @@
 
 #ifndef CONSOLEAPP_ORDERING_H
 #define CONSOLEAPP_ORDERING_H
+#include "Interface.h"
+#include "User.h"
+#include "Menu.h"
 
-
-class Ordering {
+class Ordering: public Interface{
+private:
+    User user;
+    Menu order;
+    string status;
+public:
 
 };
 
